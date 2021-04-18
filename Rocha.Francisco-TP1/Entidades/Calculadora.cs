5e8e4da02df0df4df12eef913868d9a6f194b3cc
Francisco004 <fran_25_02@hotmail.com>
2021-04-18 20:10:33 -0300

    TP 1 Calculadora FINAL

--- Rocha.Francisco-TP1/Entidades/Calculadora.cs
+++ Rocha.Francisco-TP1/Entidades/Calculadora.cs
@@ -8,6 +8,11 @@ namespace Entidades
 {
     public static class Calculadora
     {
+        /// <summary>
+        /// Valida el si el operador ingresado es "+" "-" "*" "/" 
+        /// </summary>
+        /// <param name="operador">Caracter que funcionara como operador</param>
+        /// <returns>Retorna el operador ingresado en forma de string</returns>
         private static string ValidarOperador (char operador)
         {
             string retorno = "+";
@@ -20,6 +25,13 @@ namespace Entidades
             return retorno;
         }
 
+        /// <summary>
+        /// Realiza la operacion deseada con los numeros ingresador por el usuario
+        /// </summary>
+        /// <param name="num1">Primer numero operador</param>
+        /// <param name="num2">Segundo numero operando</param>
+        /// <param name="operador">Operador con el que se realiza la cuenta</param>
+        /// <returns>Retorna el resultado de la operacion</returns>
         public static double Operar(Numero num1, Numero num2, string operador)
         {
             double resultado = 0;
